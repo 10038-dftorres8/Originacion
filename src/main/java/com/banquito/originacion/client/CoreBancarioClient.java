9f660a13141456fc62d669b727debc82c43c8bc4
dftorres8 <105184551+10038-dftorres8@users.noreply.github.com>
2025-07-07 08:27:32 -0500

    Update CoreBancarioClient.java

--- src/main/java/com/banquito/originacion/client/CoreBancarioClient.java
+++ src/main/java/com/banquito/originacion/client/CoreBancarioClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "core-bancario", url = "${app.core-bancario.url:http://localhost:8082}")
+@FeignClient(name = "core-bancario", url = "${app.core-bancario.url:http://3.129.67.241:8083}")
 public interface CoreBancarioClient {
 
     @GetMapping("/api/clientes/{tipo}/{numero}")
