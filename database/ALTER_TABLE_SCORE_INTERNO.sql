0539c4d5a9f8e8bbc230525449c530833e83774e
dftorresg22 <105184551+10038-dftorres8@users.noreply.github.com>
2025-08-02 09:49:41 -0500

    Delete ALTER_TABLE_SCORE_INTERNO.sql

--- database/ALTER_TABLE_SCORE_INTERNO.sql
+++
@@ -1,8 +0,0 @@
--- Script para agregar la columna score_interno a la tabla clientes_prospectos
--- Ejecutar en la base de datos de Originación
-
-ALTER TABLE originacion.clientes_prospectos 
-ADD COLUMN score_interno DECIMAL(10,2);
-
--- Comentario para la columna
-COMMENT ON COLUMN originacion.clientes_prospectos.score_interno IS 'Score interno del cliente obtenido del Core Bancario'; 
